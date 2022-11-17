4fcc125c05fac0b8eddbda9010d42ef69fd545bb
selva <selvanithish2014@gmail.com>
2022-11-17 11:09:19 +0530

    'server'

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('selva kumar')
+const http = require('node:http')
+
+const server = http.createServer((req,res) => {
+
+    res.writeHead(200, {'Content-type': 'application/json' })
+    res.end(JSON.stringify({
+        data : 'Hello world'
+    }))
+})
+
+server.listen(3000)
